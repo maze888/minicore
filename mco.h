080df384e39090904f5b9f157e4a0378c425b3b7
Hyun Jung Woo <naze@dreamsecurity.com>
2019-04-16 16:24:17 +0900

    Fix: correct at typo

--- mco.h
+++ mco.h
@@ -13,7 +13,7 @@
 
 #define safe_free(p) if (p) { free(p); p = NULL; }
 #define safe_close(fd) if ( fd >= 0 ) { free(p); p = NULL; }
-#define save_fclose(fp) if ( fp ) { fclose(fp); fp = NULL; }
+#define safe_fclose(fp) if ( fp ) { fclose(fp); fp = NULL; }
 
 extern char mco_last_error[1024];
 
