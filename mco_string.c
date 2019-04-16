d6bc7c7eb0a05c6b33e04d9b9ce98917ebb7e8d4
Hyun Jung Woo <naze@dreamsecurity.com>
2019-04-16 11:49:48 +0900

    Feat: add string replace api & test code for it

--- mco_string.c
+++ mco_string.c
@@ -1,6 +1,24 @@
 #include "mco_string.h"
 #include "mco_hidden.h"
 
+static int get_token_count(char *buf, const char *delim)
+{
+	int tcnt = 0;
+	char *tbuf, *token, *save_token;
+
+	tbuf = strdup(buf);
+	if ( !tbuf ) {
+		mco_set_last_error("strdup() is failed: %s (errno: %d)", strerror(errno), errno);
+		return -1;
+	}
+
+	for ( token = strtok_r(tbuf, delim, &save_token); token; token = strtok_r(NULL, delim, &save_token) ) tcnt++;
+
+	free(tbuf);
+
+	return tcnt;
+}
+
 /**
  * @brief   Add string to buffer
  *
@@ -85,4 +103,89 @@ size_t mco_right_trim(char *buf)
 	buf[len - trim] = '\0';
 
 	return trim;
+}
+
+/**
+ * @brief   Replace string of buffer
+ *
+ * @param   buf    buffer
+ * @param   bsize  buffer size
+ * @param   fstr   string to be substituted
+ * @param   rstr   substituted string
+ *
+ * @return  Length of string in buffer
+ */
+size_t mco_replace_str(char *buf, size_t bsize, const char *fstr, const char *rstr)
+{
+	char *tmp_buf = NULL;
+	size_t buf_len, fstr_len, rstr_len;
+
+	if ( !buf || bsize < 2 || !fstr || !rstr ) {
+		mco_set_last_error("invalid argument - buf: %s, bsize: %lu, fstr: %s, rstr: %s", buf ? buf : "NULL", bsize, fstr ? fstr : "NULL", rstr ? rstr : "NULL");
+		goto out;
+	}
+
+	buf_len = strlen(buf);
+	fstr_len = strlen(fstr);
+	rstr_len = strlen(rstr);
+				
+	// protect buffer overflow
+	if ( fstr_len < rstr_len ) {
+		int fcnt = get_token_count(buf, fstr);
+		if ( fcnt < 0 ) goto out;
+		
+		if ( bsize <= (buf_len + (rstr_len - fstr_len) * fcnt) ) {
+			mco_set_last_error("not enough buffer");
+			goto out;
+		}
+	}
+	
+	// protect memory overlap
+	tmp_buf = (char *)malloc(bsize);
+	if ( !tmp_buf ) {
+		mco_set_last_error("calloc() is failed: %s (errno: %d)", strerror(errno), errno);
+		goto out;
+	}
+
+	char *p = buf;
+	for ( int i = 0; i < buf_len; i++, p++) {
+		if ( buf[i] == fstr[0] ) {
+			if ( memcmp(p, fstr, fstr_len) == 0 ) {
+				memset(tmp_buf, 0x00, bsize);
+
+				if ( fstr_len == rstr_len ) {
+					memcpy(p, rstr, rstr_len);
+				}
+				else if ( fstr_len > rstr_len ) {
+					size_t plen = strlen(p + fstr_len);
+
+					memcpy(tmp_buf, p + fstr_len, plen);
+					memcpy(p + rstr_len, tmp_buf, plen);
+					memcpy(p, rstr, rstr_len);
+
+					p[plen + 1] = '\0';
+				}
+				else if ( fstr_len < rstr_len ) {
+					size_t plen = strlen(p + fstr_len);
+
+					memcpy(tmp_buf, p + fstr_len, plen);
+					memcpy(p + rstr_len, tmp_buf, plen);
+					memcpy(p, rstr, rstr_len);
+					
+					// buffer is longer
+					buf_len = buf_len + rstr_len - fstr_len;
+					p[plen + rstr_len] = '\0';
+				}
+			}
+		}
+	}
+	
+	safe_free(tmp_buf);
+	
+	return buf_len;
+	
+out:
+	safe_free(tmp_buf);
+
+	return 0;
 }
--- unit_test/string_test.h
+++ unit_test/string_test.h
@@ -6,5 +6,6 @@
 int mco_strncat_test();
 int mco_left_trim_test();
 int mco_right_trim_test();
+int mco_replace_str_test();
 
 #endif
